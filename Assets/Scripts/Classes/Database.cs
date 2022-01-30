9fb3d2162e97f1b2018cca46d9056d52513383e3
ZivFerdinand <zivenf@gmail.com>
2022-01-30 12:51:43 +0700

    Done Inverted Swipe To Roll Cube

--- Assets/Scripts/Classes/Database.cs
+++ Assets/Scripts/Classes/Database.cs
@@ -5,4 +5,19 @@ using UnityEngine;
 namespace Database
 {
     
+    public class Cameras
+    {
+        public static bool isInverted
+        {
+            set
+            {
+                PlayerPrefs.SetInt("camInverted", (value == true) ? 1 : 0);
+            }
+            get
+            {
+                int temp = PlayerPrefs.GetInt("camInverted", 0);
+                return (temp == 1) ? true : false;
+            }
+        }
+    }
 }
