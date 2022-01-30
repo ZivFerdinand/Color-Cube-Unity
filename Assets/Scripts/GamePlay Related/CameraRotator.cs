1350f09f61c940f20dde3b2e9ad500420e92f605
Keju <kelvinjulian.kj@gmail.com>
2022-01-30 17:53:05 +0700

    camera rotator guard so no double clicking

--- Assets/Scripts/GamePlay Related/CameraRotator.cs
+++ Assets/Scripts/GamePlay Related/CameraRotator.cs
@@ -2,22 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 using Database;
 
 public class CameraRotator : MonoBehaviour
 {
     float targetRotation = 0;
+    private Button cameraButton;
+
 
     public float rotateDuration;
     void Start()
     {
+        cameraButton = GameObject.Find("CameraButton").GetComponent<Button>();
+        cameraButton.enabled = true;
         targetRotation = (Database.Cameras.isInverted) ? 180 : 0;
         transform.eulerAngles = new Vector3(0, targetRotation, 0);
     }
     public void onCameraButtonClick()
     {
-        Database.Cameras.isInverted = !Database.Cameras.isInverted;
+        cameraButton.enabled = false;
         targetRotation = (targetRotation == 0) ? 180 : 0;
-        transform.LeanRotateY(targetRotation, rotateDuration).setEaseInOutSine();
+        transform.LeanRotateY(targetRotation, rotateDuration).setEaseInOutSine().setOnComplete(() =>
+        {
+            cameraButton.enabled = true; 
+            Database.Cameras.isInverted = !Database.Cameras.isInverted;
+        });
     }
 }
