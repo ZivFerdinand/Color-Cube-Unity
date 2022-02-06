64a53cdf88e7818b75087b0802854b0ed2c97ee0
ZivFerdinand <zivenf@gmail.com>
2022-02-06 13:27:17 +0700

    Done Almost Overall Level Checking 2

--- Assets/Scripts/GamePlay Related/GameManager.cs
+++ Assets/Scripts/GamePlay Related/GameManager.cs
@@ -1,20 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using TMPro;
 public class GameManager : MonoBehaviour
 {
     private List<LevelScriptableObject> levelData = new List<LevelScriptableObject>();
+    private bool[] levelColorChecker;
     private int levelTotal;
+    private int untouchedColor;
     private MeshRenderer[] cubeTileChildren;
     private MeshRenderer[] cubeSideChildren;
 
+    public TextMeshProUGUI untouchedColorStatusUI;
     public GameObject cubeTile;
     public GameObject cubeSides;
 
     public void Awake()
     {
+        untouchedColor = 0;
         Database.Functions.LoadGameData<LevelScriptableObject>(ref levelTotal, levelData, "Level SO(s)");
+        levelColorChecker = new bool[levelData[0].tileData.Length];
 
         cubeSideChildren = cubeSides.GetComponentsInChildren<MeshRenderer>();
         cubeTileChildren = cubeTile.GetComponentsInChildren<MeshRenderer>();
@@ -71,19 +76,39 @@ public class GameManager : MonoBehaviour
                     x = Color.yellow;
                     break;
             }
+
             if (levelData[0].tileData[i - 1] == TileData.Color)
+            {
+                levelColorChecker[i - 1] = false;
+                untouchedColor++;
                 cubeTileChildren[i].material.SetColor("_Color", x);
+            }
+            else
+                levelColorChecker[i - 1] = true;
         }
     }
 
 
     public void  Update()
     {
+        untouchedColorStatusUI.text = "Untouched Colors:\n" + untouchedColor.ToString();
         int touchedPlane = CheckChildCollide.collidedTileIndex;
         int touchedCubeSide = TouchingCubeArea.touchingSideIndex;
 
-        Debug.Log("TileColor: "+levelData[0].tileColor[touchedPlane] + touchedPlane.ToString());
-        Debug.Log("CubeSidesColor: "+levelData[0].cubeSidesColor[touchedCubeSide] + touchedCubeSide.ToString());
+        //Debug.Log("TileColor: "+levelData[0].tileColor[touchedPlane] + touchedPlane.ToString());
+        //Debug.Log("CubeSidesColor: "+levelData[0].cubeSidesColor[touchedCubeSide] + touchedCubeSide.ToString());
+        //Debug.Log(untouchedColor);
+
+        if(levelData[0].tileColor[touchedPlane] == levelData[0].cubeSidesColor[touchedCubeSide])
+        {
+            if(!levelColorChecker[touchedPlane])
+            {
+                Debug.Log(levelData[0].tileColor[touchedPlane].ToString() + "s are touching");
+                untouchedColor--;
+            }
+            levelColorChecker[touchedPlane] = true;
+            
+        }
     }
 
 
