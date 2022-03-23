5804286c3fc56ff5978f0fa5109f185388097aab
ZivFerdinand <zivenf@gmail.com>
2022-03-23 10:56:51 +0700

    FadeLeanTween

--- Assets/MainMenuSceneRunner.cs
+++ Assets/MainMenuSceneRunner.cs
@@ -7,12 +7,20 @@ public class MainMenuSceneRunner : MonoBehaviour
     public GameObject cubeButton;
     public Transform cubeButtonTargetPos;
     public float cubeFloatingDuration;
+    public float fadingDuration;
 
     public GameObject homeUI;
+    private CanvasGroup homeUICanvas;
     public GameObject levelCardSelectUI;
+    private CanvasGroup levelCardSelectUICanvas;
 
-    void Start()
+    void Awake()
     {
+        homeUICanvas = homeUI.GetComponent<CanvasGroup>();
+        //homeUICanvas.alpha = 1;
+
+        levelCardSelectUICanvas = levelCardSelectUI.GetComponent<CanvasGroup>();
+        //levelCardSelectUICanvas.alpha = 0;
     }
 
 
@@ -23,18 +31,54 @@ public class MainMenuSceneRunner : MonoBehaviour
 
     public void onPlayButtonClickedAnimation()
     {
-        cubeButton.LeanMoveY(cubeButtonTargetPos.position.y, cubeFloatingDuration).setEaseInOutElastic();
-        cubeButton.LeanScale(cubeButton.transform.localScale / 2, cubeFloatingDuration).setEaseInOutBounce().setOnComplete(async () =>
+        homeUI.SetActive(true);
+            homeUICanvas.alpha = 1f;
+            homeUICanvas.LeanAlpha(0, fadingDuration).setOnComplete(async() =>
+            {
+                homeUI.SetActive(false);
+                //homeUICanvas.alpha = 0;
+
+            });
+        cubeButton.LeanMoveY(cubeButtonTargetPos.position.y, cubeFloatingDuration).setEaseInOutElastic().setOnComplete(async() =>
         {
-            homeUI.SetActive(false);
-            levelCardSelectUI.SetActive(true);  
+            
+
+            levelCardSelectUI.SetActive(true);
+            levelCardSelectUICanvas.alpha = 0;
+            levelCardSelectUICanvas.LeanAlpha(1, fadingDuration).setOnComplete(async() =>
+            {
+                levelCardSelectUI.SetActive(true);
+                //levelCardSelectUICanvas.alpha = 0;
+            }); 
         });
+
+        cubeButton.LeanScale(cubeButton.transform.localScale / 2, cubeFloatingDuration).setEaseInOutBounce();
     }
     public void onReverseButtonClickedAnimation()
     {
-            levelCardSelectUI.SetActive(false);
+         levelCardSelectUI.SetActive(true);
+            
+            levelCardSelectUICanvas.alpha = 1;
+            levelCardSelectUICanvas.LeanAlpha(0, fadingDuration).setOnComplete(async() =>
+            {
+                levelCardSelectUI.SetActive(false);
+                //levelCardSelectUICanvas.alpha = 0;
+            }); 
+        
+
+           
+        cubeButton.LeanMoveY(0, cubeFloatingDuration).setEaseInOutElastic().setOnComplete(async () =>
+        {
             homeUI.SetActive(true);
-        cubeButton.LeanMoveY(0, cubeFloatingDuration).setEaseInOutElastic();
+            
+            homeUICanvas.alpha = 0;
+            homeUICanvas.LeanAlpha(1, fadingDuration).setOnComplete(async() =>
+            {
+                homeUI.SetActive(true);
+                //homeUICanvas.alpha = 1;
+            });
+            
+        });;
         cubeButton.LeanScale(cubeButton.transform.localScale * 2, cubeFloatingDuration).setEaseInOutBounce();
     }
 }
